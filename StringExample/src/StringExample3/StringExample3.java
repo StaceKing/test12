7a19503ad4bdfc548ad69b0c354a144e7ba3d778
StaceKing <staceyking2@myop.ac.nz>
2024-04-27 23:05:03 +1200

    furst

---
+++ StringExample/src/StringExample3/StringExample3.java
@@ -0,0 +1,20 @@
+package StringExample3;
+
+import java.util.Scanner;
+
+public class StringExample3 {
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		String firstName, middleName, lastName;
+		
+		System.out.println("Enter first name: ");
+		firstName = scanner.next();
+		System.out.println("Enter middle name: ");
+		middleName = scanner.next();
+		System.out.println("Enter last name: ");
+		lastName = scanner.next();
+		
+		System.out.println("Full name is: " + firstName + " " + middleName + " " + lastName);
+	}
+
+}
---
+++ StringExample/src/StringExampleTwo.java
@@ -0,0 +1,14 @@
+
+public class StringExampleTwo {
+
+	public static void main(String[] args) {
+		String firstName = "John";
+		String lastName = "Smith";
+		
+		String fullName = firstName + " " + lastName;
+		
+		System.out.println("Name: " + fullName);
+
+	}
+
+}
